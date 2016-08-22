d427789ff49955a41ad26d48d3018d96966f98e3
Dharmendra <vishdharm.181910+github@gmail.com>
2016-08-22 11:44:47 +0530

    First project

---
+++ Hackerrank/ProjectEuler+/src/org/test/programming/MultipleOf3And5.java
@@ -0,0 +1,15 @@
+package org.test.programming;
+
+import java.util.Scanner;
+
+public class MultipleOf3And5 {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		
+		scanner.close();
+		
+	}
+}
