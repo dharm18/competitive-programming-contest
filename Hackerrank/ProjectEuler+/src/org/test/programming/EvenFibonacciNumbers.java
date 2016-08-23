3337d6d37388f4bdda37e9317dfb318659dfcb66
Dharmendra <vishdharm.181910+github@gmail.com>
2016-08-23 13:04:41 +0530

    2nd problem

---
+++ Hackerrank/ProjectEuler+/src/org/test/programming/EvenFibonacciNumbers.java
@@ -0,0 +1,18 @@
+package org.test.programming;
+
+import java.util.Scanner;
+
+public class EvenFibonacciNumbers {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int t = scanner.nextInt();
+		for (int i = 0; i < t; i++) {
+			int n = scanner.nextInt();
+		}
+		
+		scanner.close();
+
+	}
+
+}
