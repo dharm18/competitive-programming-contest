44f12229f9834ac3a699bb61db1f434496b9b620
Dharmendra <vishdharm.181910+github@gmail.com>
2016-08-23 12:43:32 +0530

    - first code checked in - projec euler 1

--- Hackerrank/ProjectEuler+/src/org/test/programming/MultipleOf3And5.java
+++ Hackerrank/ProjectEuler+/src/org/test/programming/MultipleOf3And5.java
@@ -7,9 +7,35 @@ public class MultipleOf3And5 {
 	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
-		int n = scanner.nextInt();
+		int t = scanner.nextInt();
+		for (int i = 0; i < t; i++) {
+			int n = scanner.nextInt();
+			long multipleOfThree = sumOfNumbers(3,n);
+			System.out.println(multipleOfThree);
+			long multipleOfFive = sumOfNumbers(5,n);
+			System.out.println(multipleOfFive);
+			long multipleOfFiveAndThree = sumOfNumbers(15,n);
+			System.out.println(multipleOfFiveAndThree);
+			System.out.println(multipleOfThree + multipleOfFive - multipleOfFiveAndThree);
+		}
 		
 		scanner.close();
 		
 	}
+	
+	private static long sumOfNumbers(int multiplier, int limit){
+		
+		long sum = 0;
+		long maxValue = (limit-1)/multiplier;
+		System.out.println(maxValue);
+		sum = multiplier*(maxValue*(maxValue+1)/2);
+		/*for(int i=1; value<limit; i++){
+			//System.out.println(value);
+			sum +=(value);
+			value = i*multiplier;
+		}*/
+		//System.out.println("return");
+		return sum;
+	}
+	
 }
