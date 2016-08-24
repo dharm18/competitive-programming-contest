9eb1d7c0b31e7162148e5ae9da8208900ec73f91
dharm18 <vishdharm.181910@gmail.com>
2016-08-24 20:18:15 +0530

    passed all the test cases.

--- Hackerrank/ProjectEuler+/src/org/test/programming/EvenFibonacciNumbers.java
+++ Hackerrank/ProjectEuler+/src/org/test/programming/EvenFibonacciNumbers.java
@@ -8,11 +8,26 @@ public class EvenFibonacciNumbers {
 		Scanner scanner = new Scanner(System.in);
 		int t = scanner.nextInt();
 		for (int i = 0; i < t; i++) {
-			int n = scanner.nextInt();
+			long n = scanner.nextLong();
+            if(n>=10)
+			generateSum(n);
 		}
-		
+
 		scanner.close();
 
+	}
+	
+	private static void generateSum(long n){
+		long a = 1;
+		long b = 2, d;
+		long sum = 0;
+		while(b < n) {
+		    sum += b;
+		    d = a+(b<<0x01);
+		    b = d+a+b;
+		    a = d;
+		}
+		System.out.println(sum);
 	}
 
 }
