4d9ea42bd623815f8138bae983a485020a46819b
Dharmendra <vishdharm.181910+github@gmail.com>
2016-08-25 10:21:38 +0530

    local commit.

---
+++ Hackerrank/ProjectEuler+/src/org/test/programming/LargestPrimeFactor.java
@@ -0,0 +1,18 @@
+package org.test.programming;
+
+import java.util.Scanner;
+
+public class LargestPrimeFactor {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int t = scanner.nextInt();
+		for (int i = 0; i < t; i++) {
+			long n = scanner.nextLong();
+		}
+
+		scanner.close();
+	}
+
+}
