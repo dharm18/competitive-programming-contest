721262c67ff5f89932d20c4c56a1fb452139c57b
Dharmendra <vishdharm.181910+github@gmail.com>
2016-08-25 16:48:27 +0530

    code optimization for timeout error.

--- Hackerrank/ProjectEuler+/src/org/test/programming/LargestPrimeFactor.java
+++ Hackerrank/ProjectEuler+/src/org/test/programming/LargestPrimeFactor.java
@@ -5,13 +5,36 @@ import java.util.Scanner;
 public class LargestPrimeFactor {
 
 	public static void main(String[] args) {
+		long startTime = System.nanoTime();
 		Scanner scanner = new Scanner(System.in);
 		int t = scanner.nextInt();
 		for (int i = 0; i < t; i++) {
 			long n = scanner.nextLong();
+			System.out.println(largestPrimeFactor(n));
 		}
 
 		scanner.close();
+		long endTime = System.nanoTime();
+		System.out.println(((endTime-startTime)/1000000000)+" seconds");
+	}
+	
+	private static long largestPrimeFactor(long n){
+		long i=2;
+		/*for(i=2 ; i<=n; i++){
+			if(n%i==0){
+				n/=i;
+				i--;
+			}
+		}*/
+		long max=0;
+		while(n > 1){
+			while(n%i ==0){
+				n = n/i;
+				max=i;
+			}
+			i++;
+		}
+		return max;
 	}
 
 }
