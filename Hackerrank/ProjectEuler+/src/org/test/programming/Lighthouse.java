98fad26e54ecee57e19f7e686c0664d9977b2f05
Dharmendra <vishdharm.181910+github@gmail.com>
2016-09-15 11:37:00 +0530

    - code updated.

--- Hackerrank/ProjectEuler+/src/org/test/programming/Lighthouse.java
+++ Hackerrank/ProjectEuler+/src/org/test/programming/Lighthouse.java
@@ -9,6 +9,7 @@ public class Lighthouse {
 		/*Scanner scanner = new Scanner(System.in);
 		int t = scanner.nextInt();
 		boolean mat[][] = new boolean[t][t];
+		long l[][] = new long[t][t];
 		String temp = null;
 		for (int i = 0; i < t; i++) {
 			temp = scanner.nextLine();
@@ -16,11 +17,12 @@ public class Lighthouse {
 			char a[] = temp.toCharArray();
 			for (int j = 0; j < a.length; j++) {
 				mat[i][j] = a[j] == '*' ? false: true;
+				mat[i][j] = 0;
 			}
      	}*/
  		
 		
-		/*boolean mat[][] = new boolean[][]{
+		boolean mat[][] = new boolean[][]{
 				{false,false,false,false,false,false,false,false,false},
 				{false,false,false,false,true,false,false,false,false},
 				{false,false,true,true,true,true,true,false,false},
@@ -30,15 +32,16 @@ public class Lighthouse {
 				{false,false,true,true,true,true,true,false,false},
 				{false,false,false,false,true,false,false,false,false},
 				{false,false,false,false,false,false,false,false,false}
-		};*/
+		};
+		long l[][] = new long[9][9];
 		
-		boolean mat[][] = new boolean[][]{
+		/*boolean mat[][] = new boolean[][]{
 		{false,false,false,true,false},
 		{false,false,true,true,false},
 		{false,false,false,true,false},
 		{false,false,false,false,false},
 		{false,false,false,false,false},
-		};
+		};*/
 		//display matrix
 		System.out.println("\nMatrix = ");
 		for (int i = 0; i < mat.length; i++) {
@@ -48,31 +51,37 @@ public class Lighthouse {
 			System.out.println();
 		}
 		Lighthouse lt = new Lighthouse();
-		System.out.println(lt.calculateLength(mat));
-		
+		System.out.println(lt.calculateLength(mat,l));
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat[i].length; j++) {
+				System.out.print(l[i][j]+"\t");	
+			}
+			System.out.println();
+		}
 		//scanner.close();
 
 	}
 	
-	private long calculateLength(boolean mat[][]){
+	private long calculateLength(boolean mat[][],long l[][]){
 		long radius = 0;
 		long temp;
 		//System.out.println("calculate");
 		for (int i = 0; i < mat.length; i++) {
 			for (int j = 0; j < mat[i].length; j++) {
 				//System.out.print(mat[i][j]+"\t");
-				temp = lookAround(mat,i,j,0);
-				//System.out.print(temp +"\t");
-				if(temp > radius )
+				temp = lookAround(mat,i,j,1,l);
+				System.out.print(temp +"\t");
+				l[i][j] = temp;
+				if(temp > radius)
 					radius = temp;
 			}
-			//System.out.println();
+			System.out.println();
 		}
 		
 		return radius;
 	}
 	
-	public long lookAround(boolean mat[][], int row,int col,int t){
+	public long lookAround(boolean mat[][], int row,int col,int t,long l[][]){
 
 		long count = 0;
 		int rowNbr[] = new int[] {-t, -t, -t,  0, 0,  t, t, t};
@@ -80,24 +89,27 @@ public class Lighthouse {
         boolean flag = false;
         int k=0;
         for (k = 0; k < 8; ++k){
-        	if (isSafe(row + rowNbr[k], col + colNbr[k], mat) ){
-        		flag = true;
-        	}
-        	else
-        	{
+        	if (!isSafe(row + rowNbr[k], col + colNbr[k], mat) ){
         		flag = false;
         		break;
         	}
+        	else
+        	{
+        		flag = true;
+        	}
         }
-        
+        //System.out.println(flag);
+        //System.out.println(k);
         if(flag && k == 8){
-        	count += 1 + lookAround(mat,row, col,++t);
+        	count += 1 + lookAround(mat,row, col,++t,l);
         }
         
+        count += l[row][col];
         return count;
 	}
 	
 	boolean isSafe(int row, int col,boolean mat[][]) {
+		//System.out.println(row + " , " + col + "\t");
 		return (row >= 0) && (row < mat.length) && (col >= 0) && (col < mat.length) && (mat[row][col]);
 	}
 }
