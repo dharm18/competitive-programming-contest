c58be166c3a1194e0d4f652dce955cae279b05e0
Dharmendra <vishdharm.181910+github@gmail.com>
2016-09-29 16:56:59 +0530

    - hackerrank competition problem's solution.

---
+++ Hackerrank/Algorithms/src/com/dharam/algo/solutions/AsteriskExpressions.java
@@ -0,0 +1,50 @@
+package com.dharam.algo.solutions;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class AsteriskExpressions {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		for (int i = 0; i < t; i++) {
+			validExpression(sc.next());
+		}
+	}
+	
+	private static void validExpression(String exp){
+		exp = exp.replace("**", "^");
+		System.out.println(exp);
+		Stack<Character> stack = new Stack<>();
+		char []characters = exp.toCharArray();
+		char a,b;
+		for (char c : characters) {
+			if(stack.isEmpty() || !Character.isDigit(c)){
+				stack.push(c);
+			}
+			else{
+				/*while(!stack.isEmpty() && !Character.isDigit(stack.peek())){
+					
+				}*/
+				while(!stack.isEmpty()){
+					stack.pop();
+					if(!stack.isEmpty()){
+						stack.pop();
+						break;
+					}
+				}
+			}
+		}
+		
+		if(!stack.empty()){
+			System.out.println("Syntax Error");	
+		}
+		else{
+			System.out.println(stack);
+		}
+		
+	}
+
+}
