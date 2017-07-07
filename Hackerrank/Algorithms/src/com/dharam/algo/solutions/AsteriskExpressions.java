c0a991d9c3a9cd8fcede58a4f2af5e2e155794d5
dharm18 <vishdharm.181910@gmail.com>
2017-07-07 22:49:17 +0530

    resolved conflicts

--- Hackerrank/Algorithms/src/com/dharam/algo/solutions/AsteriskExpressions.java
+++ Hackerrank/Algorithms/src/com/dharam/algo/solutions/AsteriskExpressions.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.dharam.algo.solutions;
 
 import java.util.Scanner;
@@ -13,6 +12,7 @@ public class AsteriskExpressions {
 		for (int i = 0; i < t; i++) {
 			validExpression(sc.next());
 		}
+		sc.close();
 	}
 	
 	private static void validExpression(String exp){
@@ -20,7 +20,7 @@ public class AsteriskExpressions {
 		System.out.println(exp);
 		Stack<Character> stack = new Stack<>();
 		char []characters = exp.toCharArray();
-		boolean flag = false;
+		//boolean flag = false;
 		for (char c : characters) {
 			if(stack.isEmpty() || !Character.isDigit(c)){
 				stack.push(c);
@@ -44,54 +44,5 @@ public class AsteriskExpressions {
 		System.out.println(stack);
 		
 	}
-
 }
-=======
-package com.dharam.algo.solutions;
 
-import java.util.Scanner;
-import java.util.Stack;
-
-public class AsteriskExpressions {
-
-	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
-		int t = sc.nextInt();
-		for (int i = 0; i < t; i++) {
-			validExpression(sc.next());
-		}
-	}
-	
-	private static void validExpression(String exp){
-		exp = exp.replace("**", "^");
-		System.out.println(exp);
-		Stack<Character> stack = new Stack<>();
-		char []characters = exp.toCharArray();
-		boolean flag = false;
-		for (char c : characters) {
-			if(stack.isEmpty() || !Character.isDigit(c)){
-				stack.push(c);
-			}
-			else{
-				while(!stack.isEmpty()){
-					stack.pop();
-					if(!stack.isEmpty()){
-						stack.pop();
-						break;
-					}
-				}
-			}
-		}
-		
-		if(!stack.empty()){
-			System.out.println("Syntax Error");	
-		}
-		else{
-		}
-		System.out.println(stack);
-		
-	}
-
-}
->>>>>>> branch 'master' of https://github.com/dharm18/competitive-programming-contest.git
