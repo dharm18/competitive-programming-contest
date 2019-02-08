d929d86de261db8d492c60441482c54c9c7d1485
dharm18 <vishdharm.181910@gmail.com>
2019-02-08 19:17:52 +0000

    LeetCode optimised code

--- LeetCode/src/JewelsAndStones.java
+++ LeetCode/src/JewelsAndStones.java
@@ -1,4 +1,6 @@
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class JewelsAndStones {
 
@@ -7,20 +9,37 @@ public class JewelsAndStones {
 		String j =  sc.next();
 		String s = sc.next();
 		sc.close();
-		System.out.println(new JewelsAndStones().numJewelsInStones(j,s));
+		System.out.println(new JewelsAndStones().numJewelsInStonesUsingMap(j,s));
 	}
 	
+	// O(MN)
 	public int numJewelsInStones(String J, String S) {
-		char[] jewels = J.toCharArray();
-		char[] sa = S.toCharArray();
-		int count = 0;
-		for (char c : sa) {
-			for (char jw : jewels) {
+		char[] jewels = J.toCharArray(); // c1
+		char[] sa = S.toCharArray();  //c2
+		int count = 0;    //c3
+		for (char c : sa) { // O(M)
+			for (char jw : jewels) { //O(N)
 				if(c == jw)
 					count++;
 			}
 		}
  		return count;
     }
-
+	// O()
+	public int numJewelsInStonesUsingRegex(String J, String S) {
+		return S.replaceAll("[^" + J + "]", "").length();
+	}
+	
+	// O(M+N)
+	public int numJewelsInStonesUsingMap(String J, String S) {
+		Set setJ = new HashSet(); //c1
+		for(char j:J.toCharArray())  //O(N)
+			setJ.add(j);
+		int count = 0;
+		for (char c : S.toCharArray()) { //a(M)
+			if(setJ.contains(c)) //O(1)
+				count++;
+		}
+ 		return count;
+    }
 }
