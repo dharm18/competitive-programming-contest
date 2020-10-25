cfc6527f518b75304fb2abbabc1c91886ef9be58
dharm18 <vishdharm.181910@gmail.com>
2020-10-25 13:38:48 +0000

    Test 2

---
+++ CodeChef/src/main/java/com/vdharam/competitive/ReverseTheNumber.java
@@ -0,0 +1,29 @@
+package com.vdharam.competitive;
+
+import java.util.Scanner;
+
+public class ReverseTheNumber {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.hasNextInt() ? scanner.nextInt() : -1;
+        for (int i=0; i < t; i++) {
+            if( scanner.hasNextInt()) {
+                reverseNumber(scanner.nextInt());
+            }
+        }
+    }
+
+    public static void reverseNumber(int input)
+    {
+        int num = 0;
+        while(input>0)
+        {
+            num = num*10 ;
+            num = input%10 + num;
+            input = input/10;
+
+        }
+        System.out.println(num);
+    }
+}
---
+++ CodeChef/src/test/java/com/vdharam/competitive/ReverseTheNumberTest.java
@@ -0,0 +1,24 @@
+package com.vdharam.competitive;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import java.util.Scanner;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class ReverseTheNumberTest {
+
+    @Mock
+    Scanner scanner;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void main() {
+    }
+}
