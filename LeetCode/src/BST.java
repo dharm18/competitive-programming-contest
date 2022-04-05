0b68fbee134ff461038d36e7994c3a559d4b93a4
dharm18 <vishdharm.181910@gmail.com>
2022-04-05 18:09:30 +0100

    Practicing BST

---
+++ LeetCode/src/BST.java
@@ -0,0 +1,91 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.Vector;
+
+public class BST {
+
+    Node root;
+
+    class Node {
+        int data;
+        Node left;
+        Node right;
+        public Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public void insert(int data) {
+    	Node newNode = new Node(data);
+    	
+    	//check if tree is empty
+    	if(this.root == null) {
+    		this.root = newNode;
+    	}
+    	else {
+    		Node currentNode = this.root;
+    		while(true) {
+    			if(data < currentNode.data) {
+    				if(currentNode.left == null) {
+    					currentNode.left = newNode;
+    					return;
+    				}
+    				currentNode = currentNode.left;
+    			}
+    			else {
+    				if(currentNode.right == null) {
+    					currentNode.right = newNode;
+    					return;
+    				}
+    				currentNode = currentNode.right;
+    			}
+    		}
+    	}
+    }
+
+    public void remove(Node node) {
+
+    }
+
+    public void print(Node head) {
+        if(head == null)
+            return;
+        
+        print(head.left);
+        System.out.print(head.data + " ");
+        print(head.right);
+        //System.out.println();
+    }
+    
+    public void levelOrder(Node node) { // same as BFS
+    	if(node == null) 
+    		return;
+    	
+    	Queue<Node> queue = new LinkedList<Node>();
+    	queue.add(node);
+    	
+    	while(!queue.isEmpty()) {
+    		
+    		Node currentNode = queue.poll();
+    		System.out.print(currentNode.data + " -> ");
+    		if(currentNode.left != null)
+    			queue.add(currentNode.left);
+    		if(currentNode.right != null)
+    			queue.add(currentNode.right);
+    	}
+    }
+
+    public static void main(String[] args) {
+        BST bst = new BST();
+        bst.insert(9);
+        bst.insert(4);
+        bst.insert(6);
+        bst.insert(20);
+        bst.insert(170);
+        bst.insert(15);
+        bst.insert(1);
+        System.out.println("Printing tree");
+        bst.levelOrder(bst.root);
+    }
+}
