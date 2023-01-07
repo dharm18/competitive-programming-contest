f58354a10831be2eaa225e94f5e289d9ae1020d7
dharm18 <vishdharm.181910@gmail.com>
2023-01-07 10:23:45 +0000

    IsSubsequence leetcode

---
+++ LeetCode/src/IsSubsequence.java
@@ -0,0 +1,22 @@
+
+public class IsSubsequence {
+
+	public static void main(String[] args) {
+		System.out.println(new IsSubsequence().isSubsequence("abc", "ahbgdc"));
+	}
+
+	public boolean isSubsequence(String s, String t) {
+        int sindex=0, tindex=0;
+        while(sindex < s.length() && tindex < t.length()) {
+            if(s.charAt(sindex) == t.charAt(tindex)) {
+                sindex++;
+                tindex++;
+            }
+            else{
+                tindex++;
+            }
+        }
+
+        return sindex == s.length();
+    }
+}
