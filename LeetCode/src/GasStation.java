d1f1bd6b4b4c561463bc1fe860f51d3488d19185
dharm18 <vishdharm.181910@gmail.com>
2023-01-09 00:46:13 +0000

    Testing

--- LeetCode/src/GasStation.java
+++ LeetCode/src/GasStation.java
@@ -4,6 +4,9 @@ public class GasStation {
 	public static void main(String[] args) {
 		//System.out.println(new GasStation().canCompleteCircuit(new int[] {1,2,3,4,5}, new int[] {3,4,5,1,2}));
 		System.out.println(new GasStation().canCompleteCircuit(new int[] {5,1,2,3,4}, new int[] {4,4,1,5,1}));
+		
+		
+		
 	}
 	
 	 public int canCompleteCircuit(int[] gas, int[] cost) {
